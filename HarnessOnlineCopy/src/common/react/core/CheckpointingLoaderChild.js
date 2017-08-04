999ee9cc36b806e86cbc6c6bc9923814d834df24
Justin <jual6332@colorado.edu>
2017-08-04 08:07:23 -0600

    Final Update

--- HarnessOnlineCopy/src/common/react/core/CheckpointingLoaderChild.js
+++ HarnessOnlineCopy/src/common/react/core/CheckpointingLoaderChild.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {$GreyLight} from './Variables';
 
-const NewProgress = styled.div`
+const Progress = styled.div`
   border: 2px;
   width: 100%;
   max-width: 800px;
@@ -13,6 +13,18 @@ const NewProgress = styled.div`
   left: 4%;
 `;
 
+const ListItem = styled.li`
+  background-color: #0e90d2;
+  display: table-cell;
+  position: relative;
+  line-height: 20px;
+  z-index: -3;
+  color: #FFF;
+  text-align: left;
+  transition: background-color 0.75s, color 0.5s;
+  border: solid 5px #0e90d2;
+`;
+
 class CheckpointingLoaderChild extends React.Component{
   constructor(props) { 
   	super(props); 
@@ -20,7 +32,13 @@ class CheckpointingLoaderChild extends React.Component{
 
   render() { 
     return ( 
-	  <NewProgress></NewProgress>
+	  <Progress>
+	  	<ul>
+		  	<ListItem>1</ListItem>
+		  	<ListItem>2</ListItem>
+		  	<ListItem>3</ListItem>
+		</ul>
+	  </Progress>
     ); 
   }
 }
